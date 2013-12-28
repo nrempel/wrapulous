480bf9859a189a4442f408634dc25ff58fe02fbd
Nicholas Rempel <nbrempel@gmail.com>
2013-12-28 14:15:33 -0800

    add middleware

---
+++ middleware.js
@@ -0,0 +1,10 @@
+exports.defaultContentType = function (req, res, next) {
+    req.headers['content-type'] = req.headers['content-type'] || 'application/json';
+    if (req.headers['content-type'] !== 'application/json') {
+        res.send(
+            400,
+            'content type "application/json" expected but found "'+ req.headers['content-type'] + '" instead');
+    } else {
+        next();
+    }
+};
