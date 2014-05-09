cb6c2e28da98be6c4fa7c2c211b61c434abc19f1
Nicholas Rempel <nick@h2odevworks.ca>
2014-05-08 20:21:40 -0700

    debug

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -6,13 +6,15 @@ exports.shorten_url = function (req, res) {
   // var request_json = JSON.stringify({
   //   destination: url
   // });
-  //
-  // request.post({
-  //   url: 'http://api.wrapulous.com/api/v0/links/',
-  //   json: request_json
-  // }, function (err, res, body) {
-  //
-  // });
+
+  var request_json = '{"test": "test"}';
+
+  request.post({
+    url: 'http://api.wrapulous.com/api/v0/links/',
+    json: request_json
+  }, function (err, res, body) {
+
+  });
 
 
 };
