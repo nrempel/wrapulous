768799a13432662023032d27173cfe01c77b9537
Nicholas Rempel <nick@h2odevworks.ca>
2014-05-14 23:33:50 -0700

    redesign and shabam

--- public/js/home.js
+++ public/js/home.js
@@ -13,17 +13,33 @@ $(function () {
       })
       .done(function(msg) {
         console.log(msg);
+        // Glow some stuff
         shortener.addClass('glow-green');
         setTimeout(function(){
           shortener.removeClass('glow-green');
-        }, 1000);
+        }, 100);
+
+        console.log(msg);
+
+        // TODO: Store which proto in db
+
+        // Update dom to match response
+        $("#short-link").attr('href', 'http://' + msg.url + '/' + msg.tag)
+          .text('http://' + msg.url + '/' + msg.tag);
+
+        $("#tracking-data").attr('href', 'http://api.wrapulous.com/api/v0/links/' + msg.tag + '/events/')
+          .text('http://api.wrapulous.com/api/v0/links/' + msg.tag + '/events/');
+
       })
       .fail(function(msg) {
         console.log(msg);
+        // Glow some stuff
         shortener.addClass('glow-red');
         setTimeout(function(){
           shortener.removeClass('glow-red');
-        }, 1000);
+        }, 100);
+
+
       });
     }
   });
