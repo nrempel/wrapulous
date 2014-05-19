1f64d69ac0a3070f1d9c062d04fa3bdaaa2b1cc9
Nicholas Rempel <nick@h2odevworks.ca>
2014-05-19 14:19:17 -0700

    add submit button

--- public/js/home.js
+++ public/js/home.js
@@ -1,46 +1,60 @@
-// On enter press
 $(function () {
-  var shortener = $('#shortener');
-  shortener.keypress(function (event) {
+
+  var shortenerButton = $('#shortener-button');
+  var shortenerLadda = $('#shortener-button').ladda();
+  var shortenerInput = $('#shortener-input');
+
+  var shortenUrl = function () {
+    var url = shortenerInput.val();
+    $.ajax({
+      type: 'POST',
+      contentType: 'application/json',
+      url: '/ajax/shorten_url/',
+      data: JSON.stringify({destination: url})
+    })
+    .done(function(msg) {
+      console.log(msg);
+      // Glow some stuff
+      shortenerInput.addClass('glow-green');
+      setTimeout(function(){
+        shortenerInput.removeClass('glow-green');
+      }, 100);
+
+      console.log(msg);
+
+      // TODO: Store which proto in db
+
+      // Update dom to match response
+      $("#short-link").attr('href', 'http://' + msg.url + '/' + msg.tag)
+        .text('http://' + msg.url + '/' + msg.tag);
+
+      $("#tracking-data").attr('href', 'http://api.wrapulous.com/api/v0/links/' + msg.tag + '/events/')
+        .text('http://api.wrapulous.com/api/v0/links/' + msg.tag + '/events/');
+    })
+    .fail(function(msg) {
+      console.log(msg);
+      // Glow some stuff
+      shortenerInput.addClass('glow-red');
+      setTimeout(function(){
+        shortenerInput.removeClass('glow-red');
+      }, 100);
+    })
+    .always(function () {
+      shortenerLadda.ladda('stop');
+    });
+  };
+
+  // On button press
+  shortenerButton.click(function () {
+    shortenerLadda.ladda('start');
+    shortenUrl();
+  });
+
+  // On enter press
+  shortenerInput.keypress(function (event) {
     if (event.which == 13) {
-      var url = $("#shortener").val();
-
-      $.ajax({
-        type: 'POST',
-        contentType: 'application/json',
-        url: '/ajax/shorten_url/',
-        data: JSON.stringify({destination: url})
-      })
-      .done(function(msg) {
-        console.log(msg);
-        // Glow some stuff
-        shortener.addClass('glow-green');
-        setTimeout(function(){
-          shortener.removeClass('glow-green');
-        }, 100);
-
-        console.log(msg);
-
-        // TODO: Store which proto in db
-
-        // Update dom to match response
-        $("#short-link").attr('href', 'http://' + msg.url + '/' + msg.tag)
-          .text('http://' + msg.url + '/' + msg.tag);
-
-        $("#tracking-data").attr('href', 'http://api.wrapulous.com/api/v0/links/' + msg.tag + '/events/')
-          .text('http://api.wrapulous.com/api/v0/links/' + msg.tag + '/events/');
-
-      })
-      .fail(function(msg) {
-        console.log(msg);
-        // Glow some stuff
-        shortener.addClass('glow-red');
-        setTimeout(function(){
-          shortener.removeClass('glow-red');
-        }, 100);
-
-
-      });
+      shortenerLadda.ladda('start');
+      shortenUrl();
     }
   });
 });
