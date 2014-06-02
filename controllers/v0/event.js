d972537fbd251092ce3b7c2b56a908f69f95234a
Nicholas Rempel <nick@h2odevworks.ca>
2014-06-01 19:46:13 -0700

    fix event list

--- controllers/v0/event.js
+++ controllers/v0/event.js
@@ -1,6 +1,9 @@
-var Event = require('../../models/event.js');
+var Link = require('../../models/link.js');
 
 exports.list = function (req, res) {
+
+	var id = req.params.linkId;
+
 	var limit = Number(req.query.limit) || 100;
 	var offset = Number(req.query.offset) || 0;
 	var sortBy = req.query.sortBy || 'created';
@@ -18,9 +21,9 @@ exports.list = function (req, res) {
 		sort[sortBy] = -1;
 	}
 
-	query = Event.find().sort(sort).skip(offset).limit(limit);
-	query.exec(function (err, events) {
+	query = Link.findOne({tag: id}).sort(sort).skip(offset).limit(limit);
+	query.exec(function (err, link) {
 		if (err) { console.log(err); }
-		res.send(events);
+		res.send(link.events);
 	});
 };
