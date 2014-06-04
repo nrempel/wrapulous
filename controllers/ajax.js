fb7d41a3d8f6140a4686520e0321bd378d847730
Nicholas Rempel <nick@h2odevworks.ca>
2014-06-03 20:17:33 -0700

    debugging

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -6,6 +6,9 @@ exports.shorten_url = function (req, res) {
     url: 'http://api.wrapulous.com/api/v0/links/',
     json: {destination: url}
   }, function (error, response, body) {
+
+    console.log(response);
+
     res.send(
       response.statusCode,
       {
