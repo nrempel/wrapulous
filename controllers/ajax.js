cc9c1d81b7bd5ffa2c638e4a9d13458f2541eb92
Nicholas Rempel <nick@h2odevworks.ca>
2014-06-03 20:28:09 -0700

    forgot the @ sign

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -6,9 +6,6 @@ exports.shorten_url = function (req, res) {
     url: 'http://api.wrapulous.com/api/v0/links/',
     json: {destination: url}
   }, function (error, response, body) {
-
-    console.log(response);
-
     res.send(
       response.statusCode,
       {
