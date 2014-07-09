f206398e273bbc43d8e395d00d5eddbb04cc04de
Nicholas Rempel <nbrempel@gmail.com>
2014-07-09 16:46:31 -0700

    forgot to update ajax handler

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -3,7 +3,7 @@ var request = require('request');
 exports.shorten_url = function (req, res) {
   var url = req.body.destination;
   request.post({
-    url: 'http://api.wrapulous.com/api/v0/links/',
+    url: 'http://api.wrapulous.com/v0/links/',
     json: {destination: url}
   }, function (error, response, body) {
     res.send(
